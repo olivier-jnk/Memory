bc3f4ce5bb377d4ad53dfd860e281579acd62f2a
olivier-jnk <oliviertrouvain01@gmail.com>
2024-01-23 13:30:12 +0100

    Ajout du systeme de choix de la taille du terrain

--- main.js
+++ main.js
@@ -1,140 +1,182 @@
-var images = [
+var images0 = [
     { chemin: "Images/carte1.png", alt: "", valeur: 0, numéro: 0 },
     { chemin: "Images/carte2.png", alt: "", valeur: 0, numéro: 1 },
     { chemin: "Images/carte3.png", alt: "", valeur: 0, numéro: 2 },
     { chemin: "Images/carte4.png", alt: "", valeur: 0, numéro: 3 },
     { chemin: "Images/carte5.png", alt: "", valeur: 0, numéro: 4 },
     { chemin: "Images/carte6.png", alt: "", valeur: 0, numéro: 5 },
-    // { chemin: "Images/carte7.png", alt: "", valeur: 0, numéro: 6 },
-    // { chemin: "Images/carte8.png", alt: "", valeur: 0, numéro: 7 }
 ];
 
-function winGame (){
-    setTimeout(function() { 
-        if (coup <= 6){
-            alert('TRICHEUR !')
-            // + message perso pour afficher le nombre de coups
-        }
-        else if (coup > 6 || coup < 10){
-            alert("Vous etes de ceux qui ont l'esprit vif !")
-        }
-        else if (coup > 11 || coup < 15){
-            alert('Pas trop mal...')
-        }
-        else{
-            alert('0/20')
-        }
-        valeur = 0;
-        cartesCliques = [];
-        window.location.href = 'index.html';
-    }, 1000);
+var images1 = [
+    { chemin: "Images/carte1.png", alt: "", valeur: 0, numéro: 0 },
+    { chemin: "Images/carte2.png", alt: "", valeur: 0, numéro: 1 },
+    { chemin: "Images/carte3.png", alt: "", valeur: 0, numéro: 2 },
+    { chemin: "Images/carte4.png", alt: "", valeur: 0, numéro: 3 },
+    { chemin: "Images/carte5.png", alt: "", valeur: 0, numéro: 4 },
+    { chemin: "Images/carte6.png", alt: "", valeur: 0, numéro: 5 },
+    { chemin: "Images/carte6.png", alt: "", valeur: 0, numéro: 6 },
+    { chemin: "Images/carte6.png", alt: "", valeur: 0, numéro: 7 },
+    { chemin: "Images/carte6.png", alt: "", valeur: 0, numéro: 8 },
+    //changer les images
+]
+const images = [images0, images1];
 
+window.onload = function () {
+    console.log("lancement de la page")
+    choixTaille("0");
 }
+
+function choixTaille(taille) {
+
+    // let choixTerrainV = images0;
+    // console.log(choixTerrainV + "Choix terrain fInit")
+    let choixTerrainV = images[taille];
+
+    // if(taille){
+    //     
+    //     console.log('lancement du if')
+    //     return choixTerrainV;
+    // }
+    console.log(choixTerrainV + " Choix Terrain")
+    partie (choixTerrainV);
+    return choixTerrainV;
+}
+
+const choixTerrain = images0;
+// const choixTerrain2 = "images" + images0;
 
 function reload (){
     window.location.href= 'index.html';
 }
 
-const cartesDisponibles = images.filter(carte => carte.valeur < 2);
-const cartesSelectionnees = {};
 
-for (let i = 0; i < images.length * 2; i++) {
-    let liste = document.getElementById('cartes');
-    if (cartesDisponibles.length > 0) {
-        const indexAleatoire = Math.floor(Math.random() * cartesDisponibles.length);
-        const randomCard = cartesDisponibles[indexAleatoire];
+function partie (choixTerrain){
 
-        randomCard.valeur += 1;
-        let imageCarte = document.createElement('li');
-        imageCarte.innerHTML = `<img src="${randomCard.chemin}" alt="${randomCard.alt}">`;
-        imageCarte.id = randomCard.numéro;
-        imageCarte.className = 'invisible';
-        liste.appendChild(imageCarte);
+    const cartesDisponibles = choixTerrain.filter(carte => carte.valeur < 2);
+    const cartesSelectionnees = {};
 
-        if (cartesSelectionnees[randomCard.chemin]) {
-            cartesSelectionnees[randomCard.chemin] += 1;
+    for (let i = 0; i < choixTerrain.length * 2; i++) {
+        let liste = document.getElementById('cartes');
+        if (cartesDisponibles.length > 0) {
+            const indexAleatoire = Math.floor(Math.random() * cartesDisponibles.length);
+            const randomCard = cartesDisponibles[indexAleatoire];
+
+            randomCard.valeur += 1;
+            let imageCarte = document.createElement('li');
+            imageCarte.innerHTML = `<img src="${randomCard.chemin}" alt="${randomCard.alt}">`;
+            imageCarte.id = randomCard.numéro;
+            imageCarte.className = 'invisible';
+            liste.appendChild(imageCarte);
+
+            if (cartesSelectionnees[randomCard.chemin]) {
+                cartesSelectionnees[randomCard.chemin] += 1;
+            } else {
+                cartesSelectionnees[randomCard.chemin] = 1;
+            }
+
+            if (cartesSelectionnees[randomCard.chemin] === 2) {
+                cartesDisponibles.splice(indexAleatoire, 1);
+            }
         } else {
-            cartesSelectionnees[randomCard.chemin] = 1;
+            break; 
         }
-
-        if (cartesSelectionnees[randomCard.chemin] === 2) {
-            cartesDisponibles.splice(indexAleatoire, 1);
-        }
-    } else {
-        break; 
     }
-}
 
-let valeur = 0;
-let cartesCliques = [];
-let coup = 0; 
-let bonnePioche = 0; 
+    let valeur = 0;
+    let cartesCliques = [];
+    let coup = 0; 
+    let bonnePioche = 0; 
 
-var cartesClass = document.getElementsByClassName('invisible');
+    var cartesClass = document.getElementsByClassName('invisible');
 
-// Boucle for peut etre pas la plus adaptée. pour le addeventlistener oui, mais pour le reste...
-for (var i = 0; i < cartesClass.length; i++) {
-    cartesClass[i].addEventListener('click', function() {
+    // Boucle for peut etre pas la plus adaptée. pour le addeventlistener oui, mais pour le reste...
+    for (var i = 0; i < cartesClass.length; i++) {
+        cartesClass[i].addEventListener('click', function() {
 
-        if (valeur < 2){ // modifier val
-            //clear les firstclicks.
-            this.classList.remove('invisible');
-            // this.classList.removeEventListener('click')
-            // this.classList.removeEventListener('click', arguments.callee);
-            // this.classList.add('firstclick')
-            // Peut etre plus besoin du passage en 'firstclick' avec le tableau cartesCliques.
-            valeur = valeur + 1;
-            cartesCliques.push(this);
-            carteVerificator();
-        }    
+            if (valeur < 2){ // modifier val
+                //clear les firstclicks.
+                this.classList.remove('invisible');
+                // this.classList.removeEventListener('click')
+                // this.classList.removeEventListener('click', arguments.callee);
+                // this.classList.add('firstclick')
+                // Peut etre plus besoin du passage en 'firstclick' avec le tableau cartesCliques.
+                valeur = valeur + 1;
+                cartesCliques.push(this);
+                carteVerificator();
+            }    
 
-    });
+        });
+            
+    }
+
+    function carteVerificator() {
+        if (cartesCliques.length === 2){ // Et que deux cartes selectionnés sont pas les mm. sinon remettre la classe invisible a ces deux ci.
+            var carteclicked1 = cartesCliques[0];
+            var carteclicked2 = cartesCliques[1];
         
-}
+            var idCarteClicked1 = carteclicked1.id
+            var idCarteClicked2 = carteclicked2.id
 
-function carteVerificator() {
-    if (cartesCliques.length === 2){ // Et que deux cartes selectionnés sont pas les mm. sinon remettre la classe invisible a ces deux ci.
-        var carteclicked1 = cartesCliques[0];
-        var carteclicked2 = cartesCliques[1];
-    
-        var idCarteClicked1 = carteclicked1.id
-        var idCarteClicked2 = carteclicked2.id
-
-        if (idCarteClicked1 === idCarteClicked2){
-            bonnePioche = bonnePioche + 1;
-            // carteclicked1.removeEventListener('click', arguments.callee); //NE FONCTIONNE PAS !
-            // carteclicked2.removeEventListener('click', arguments.callee); //NE FONCTIONNE PAS !
-            // Trouver un vrai moyen de remove le event listener click.
-            carteclicked1.classList.remove('firstclick')
-            carteclicked1.classList.add('found')
-            carteclicked2.classList.remove('firstclick')
-            carteclicked2.classList.add('found')
-            console.log(bonnePioche + 'bonnepioche')
-        } else {
-            console.log('Cartes ne correspondent pas !')
-            setTimeout(function() { // delai d'une seconde pour que la deuxieme carte se revele, meme si elle ne correspond pas a la premiere.
-                // carteclicked1.classList.remove('firstclick')
-                carteclicked1.classList.add('invisible')
-                // carteclicked2.classList.remove('firstclick')
-                carteclicked2.classList.add('invisible')
-            }, 1000);
-        }
-        if (bonnePioche >= images.length){
-            coup = coup + 1;
-            winGame ();
-            console.log('roadtoWin')
-        }else{
-            setTimeout(function() {
-                // Réinitialisez des valeurs et du tabl
-                valeur = 0;
-                cartesCliques = [];
+            if (idCarteClicked1 === idCarteClicked2){
+                bonnePioche = bonnePioche + 1;
+                // carteclicked1.removeEventListener('click', arguments.callee); //NE FONCTIONNE PAS !
+                // carteclicked2.removeEventListener('click', arguments.callee); //NE FONCTIONNE PAS !
+                // Trouver un vrai moyen de remove le event listener click.
+                carteclicked1.classList.remove('firstclick')
+                carteclicked1.classList.add('found')
+                carteclicked2.classList.remove('firstclick')
+                carteclicked2.classList.add('found')
+                console.log(bonnePioche + 'bonnepioche')
+            } else {
+                console.log('Cartes ne correspondent pas !')
+                setTimeout(function() { // delai d'une seconde pour que la deuxieme carte se revele, meme si elle ne correspond pas a la premiere.
+                    // carteclicked1.classList.remove('firstclick')
+                    carteclicked1.classList.add('invisible')
+                    // carteclicked2.classList.remove('firstclick')
+                    carteclicked2.classList.add('invisible')
+                }, 1000);
+            }
+            if (bonnePioche >= choixTerrain.length){
                 coup = coup + 1;
-                console.log(coup + 'nombre de coups')
-                console.log(images.length + 'images.lenght')
-            }, 1000);
+                winGame ();
+                console.log('roadtoWin')
+            }else{
+                setTimeout(function() {
+                    // Réinitialisez des valeurs et du tabl
+                    valeur = 0;
+                    cartesCliques = [];
+                    coup = coup + 1;
+                    console.log(coup + 'nombre de coups')
+                    console.log(choixTerrain.length + 'choix.lenght')
+                }, 1000);
+            }
         }
     }
+
+    function winGame (){
+        setTimeout(function() {
+            if (coup <= 6){
+                alert('TRICHEUR !')
+                // + message perso pour afficher le nombre de coups
+            }
+            else if (coup > 6 || coup < 10){
+                alert("Vous etes de ceux qui ont l'esprit vif !")
+            }
+            else if (coup > 11 || coup < 15){
+                alert('Pas trop mal...')
+            }
+            else{
+                alert('0/20')
+            }
+            valeur = 0;
+            cartesCliques = [];
+            window.location.href = 'index.html';
+        }, 1000);
+    
+    }
+
 }
+
 
 // A ajouter
 
